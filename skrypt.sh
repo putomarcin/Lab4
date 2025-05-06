5985ba389ccde27648453c7fd097a5cba4ad8ad9
putomarcin <marcinputo14@gmail.com>
2025-05-07 00:28:23 +0200

    dodalem generowanie logow

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
-    date
+elif [[ "$1" == "--logs" ]]; then
+    num_files=${2:-100}
+    for ((i=1; i<=num_files; i++)); do
+        filename="log${i}.txt"
+        echo -e "$filename\n$(basename "$0")\n$(date)" > "$filename"
+    done
 fi
