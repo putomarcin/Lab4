8dfc660f3e8a597b4881f2c023a349764093a5eb
putomarcin <marcinputo14@gmail.com>
2025-05-07 00:34:50 +0200

    dodano pomoc --help

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-elif [[ "$1" == "--logs" ]]; then
-    num_files=${2:-100}
-    for ((i=1; i<=num_files; i++)); do
-        filename="log${i}.txt"
-        echo -e "$filename\n$(basename "$0")\n$(date)" > "$filename"
-    done
+elif [[ "$1" == "--help" ]]; then
+    echo "--date        Wyświetla dzisiejszą datę"
+    echo "--logs [N]    Tworzy N plików log (domyślnie 100)"
+    echo "--help        Pokazuje dostępne opcje"
 fi
+
