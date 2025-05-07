ef7abb29f0d9e20fb0f18abb0dfbf2b92323ce19
putomarcin <marcinputo14@gmail.com>
2025-05-07 15:58:07 +0200

    dodano --error i -e super

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,10 @@
-elif [[ "$1" == "--init" ]]; then
-    REPO_URL="https://github.com/putomarcin/Lab4.git"
-    git clone "$REPO_URL"
-    REPO_NAME=$(basename "$REPO_URL" .git)
-    export PATH="$PWD/$REPO_NAME:$PATH"
-    echo "Dodano $PWD/$REPO_NAME do PATH"
+elif [[ "$1" == "--error" || "$1" == "-e" ]]; then
+    num_files=${2:-100}
+    mkdir -p error
+    for ((i=1; i<=num_files; i++)); do
+        fname="error/error${i}.txt"
+        echo -e "$fname\n$(basename "$0")\n$(date)" > "$fname"
+    done
+
 
 
