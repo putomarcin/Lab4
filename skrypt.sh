db58591561ea3d6fc0309d412dde0164221ab4af
putomarcin <marcinputo14@gmail.com>
2025-05-07 15:43:10 +0200

    dodalem aliasy -d -l -h

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
 
-elif [[ "$1" == "--help" ]]; then
-    echo "--date        Wyświetla dzisiejszą datę"
-    echo "--logs [N]    Tworzy N plików log (domyślnie 100)"
-    echo "--help        Pokazuje dostępne opcje"
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
+    date
+
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+    num_files=${2:-100}
+    for ((i=1; i<=num_files; i++)); do
+        filename="log${i}.txt"
+        echo -e "$filename\n$(basename "$0")\n$(date)" > "$filename"
+    done
+
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "--date / -d        Wyświetla dzisiejszą datę"
+    echo "--logs [N] / -l N  Tworzy N plików log (domyślnie 100)"
+    echo "--error [N] / -e N Tworzy N plików error"
+    echo "--help / -h        Pokazuje dostępne opcje"
+    echo "--init             Klonuje repozytorium i ustawia PATH"
+
+else
+    echo "Nieznana opcja. Użyj --help lub -h."
 fi
 
